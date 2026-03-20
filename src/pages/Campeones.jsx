064bb0e9692a30e95967b1cd8b1dbb9047d38119
ccuascota-beep <carloscuascota2007@gmail.com>
2026-03-20 15:45:28 -0500

    Merge remote-tracking branch 'origin/main'

--- src/pages/Campeones.jsx
+++ src/pages/Campeones.jsx
@@ -130,7 +130,7 @@ export default function Campeones() {
 
                                 <h1 className="text-white font-bold text-xl">MARLUKMANIA</h1>
 
-                                <div className="flex gap-5">
+                                <div className="flex flex-col gap-3">
 
                                     <a href="https://www.instagram.com/michaelstevem/?hl=es-la" target="_blank" rel="noopener noreferrer">
                                         <FaInstagram className="text-pink-500 w-6 h-6 hover:scale-110 transition cursor-pointer" />
@@ -138,18 +138,6 @@ export default function Campeones() {
 
                                     <a href="https://www.tiktok.com/@michaelsteve?lang=es" target="_blank" rel="noopener noreferrer">
                                         <FaTiktok className="text-white w-6 h-6 hover:scale-110 transition cursor-pointer" />
-                                    </a>
-
-                                    <a href="https://kick.com/marluking_on_fire" target="_blank" rel="noopener noreferrer">
-                                        <SiKick className="text-green-500 w-6 h-6 hover:scale-110 transition cursor-pointer" />
-                                    </a>
-
-                                    <a href="https://www.youtube.com/@MarluKingExclusive" target="_blank" rel="noopener noreferrer">
-                                        <FaYoutube className="text-red-600 w-6 h-6 hover:scale-110 transition cursor-pointer" />
-                                    </a>
-
-                                    <a href="https://www.youtube.com/@SUERTE-TV/featured" target="_blank" rel="noopener noreferrer">
-                                        <FaYoutube className="text-red-600 w-6 h-6 hover:scale-110 transition cursor-pointer" />
                                     </a>
 
                                 </div>
